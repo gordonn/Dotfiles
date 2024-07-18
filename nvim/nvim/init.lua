ef86be66fbff6c3ab0a817aa2f95ffa4c9ad4b56
Gordon <tofugordon294@gmail.com>
2024-07-18 05:40:41 -0700

    tweaks

---
+++ nvim/nvim/init.lua
@@ -0,0 +1,7 @@
+require("gordon.core")
+require("gordon.lazy")
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = { "*" },
+  command = [[%s/\s\+$//e]],
+})
---
+++ nvim/nvim/lua/gordon/plugins/alpha.lua
@@ -0,0 +1,42 @@
+return {
+  "goolord/alpha-nvim",
+  event = "VimEnter",
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
+
+    dashboard.section.header.val = {
+      [[]],
+      [[]],
+      [[]],
+      [[]],
+      [[]],
+      [[]],
+      [[]],
+      [[]],
+      [[                                                                       ]],
+      [[  ██████   █████                   █████   █████  ███                  ]],
+      [[ ░░██████ ░░███                   ░░███   ░░███  ░░░                   ]],
+      [[  ░███░███ ░███   ██████   ██████  ░███    ░███  ████  █████████████   ]],
+      [[  ░███░░███░███  ███░░███ ███░░███ ░███    ░███ ░░███ ░░███░░███░░███  ]],
+      [[  ░███ ░░██████ ░███████ ░███ ░███ ░░███   ███   ░███  ░███ ░███ ░███  ]],
+      [[  ░███  ░░█████ ░███░░░  ░███ ░███  ░░░█████░    ░███  ░███ ░███ ░███  ]],
+      [[  █████  ░░█████░░██████ ░░██████     ░░███      █████ █████░███ █████ ]],
+      [[ ░░░░░    ░░░░░  ░░░░░░   ░░░░░░       ░░░      ░░░░░ ░░░░░ ░░░ ░░░░░  ]],
+      [[                                                                       ]],
+    }
+
+    dashboard.section.buttons.val = {
+      dashboard.button("e", "   New File", "<cmd>ene<CR>"),
+      dashboard.button("SPC ee", "   File Explorer", "<cmd>NvimTreeToggle<CR>"),
+      dashboard.button("SPC ff", "   Find File", "<cmd>Telescope find_files<CR>"),
+      dashboard.button("SPC wr", "   Restore Session", "<cmd>SessionRestore<CR>"),
+    }
+
+    -- Send config to alpha
+    alpha.setup(dashboard.opts)
+
+    -- Disable folding on alpha buffer
+    vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+  end,
+}
